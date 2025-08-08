cd6651e1c67e1def9a495f45dee2b670e634978f
Gb020207 <gonezen123@gmail.com>
2025-08-08 15:05:13 -0300

    se realizo task.controllers.js

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -37,3 +37,98 @@ export const createUser = async (req,res) => {
     }
     
 };
+export const getAllTasks = async (req, res) => {
+    try {
+        const tasks = await Task.findAll();
+        return res.json({
+            count: tasks.length,
+            data: tasks
+        })
+
+    } catch (error) {
+        console.log(error);
+        return res.status(404).json({
+              msg:"No se encontraron tareas"
+        }
+          
+        )
+    }
+};
+export const getTaskById = async (req, res) => {
+    const {id} = req.params;
+    try {
+        const task = await Task.findByPk(id);
+        if(!task){
+            return res.status(404).json({
+                msg:"No se encontro la tarea"
+            })
+        }
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            msg:"No se pudo encontrar la tarea"
+        })
+    }
+};
+export const updateTaskById = async (req, res) => {
+    const {id} = req.params;
+    const {title,description} = req.body;
+    try {
+        const task = await Task.findByPk(id);
+        if(!task){
+            return res.status(404).json({
+                msg:"No se encontro la tarea"
+            })
+        }
+        if (title === undefined || title === ""){
+            return res.status(400).json({
+                msg:"Title no puede ser nulo"
+            })
+        };
+        if (description === undefined || description === ""){
+            return res.status(400).json({
+                msg:"Description no puede ser nulo"
+            })
+        };
+        await Task.update({title,description},{where:{id}});
+        return res.status(200).json({
+            msg:"Tarea actualizada correctamente"
+        })
+
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            msg:"No se pudo actualizar la tarea"
+        })
+    }
+
+
+
+
+
+}
+
+export const deleteTaskById = async (req, res) => {
+    const {id} = req.params;
+    try {
+        const task = await Task.findByPk(id);
+        if(!task){
+            return res.status(404).json({
+                msg:"No se encontro la tarea"
+            })
+        }
+        await Task.destroy({where:{id}});
+        return res.status(200 .json({
+            msg:"Tarea eliminada correctamente"}))
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            msg:"No se pudo eliminar la tarea"
+        })
+    }
+
+
+
+}
+
